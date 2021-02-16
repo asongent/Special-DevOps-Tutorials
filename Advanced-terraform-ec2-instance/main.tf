067afbff85ecff1a8b278b8b53a16a99ee2d5881
asongent <59109755+asongent@users.noreply.github.com>
2021-02-16 08:14:34 -0500

    Update main.tf

--- Advanced-terraform-ec2-instance/main.tf
+++ Advanced-terraform-ec2-instance/main.tf
@@ -46,6 +46,14 @@ resource "aws_security_group" "test-Instance" {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+ 
+  }
+  ingress {
+    from_port = 0
+    to_port = 65535
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   ingress {
     from_port = 443
